2aedf535e073d950e32f2dbb41cc66b571e0d338
BrvAlexis <brevieralexis@gmail.com>
2024-03-20 21:16:59 +0100

    mailerpreview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
+    def reservation_email
+        reservation = Reservation.first
+        UserMailer.reservation_email(reservation)
+    end
+
 end
