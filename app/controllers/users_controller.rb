62e7613513b6d84d7113d688166a4a1b7f968cc6
lamrabt <lamrabtyassin3@gmail.com>
2024-03-20 18:10:31 +0100

    view profile 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @number = rand(1..20)
-    @nombre = rand(1..20)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_131730) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "reservations", force: :cascade do |t|
     t.bigint "workout_id"
     t.bigint "user_id"
