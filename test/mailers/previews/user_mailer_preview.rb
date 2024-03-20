93cfb9c4aca654361a66fde4da77ff3697e681fa
BrvAlexis <brevieralexis@gmail.com>
2024-03-20 22:18:40 +0100

    status preview ok

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
+    def reservation_accepted_email
+      
+        reservation = Reservation.first
+        reservation.status = :accepted
+        reservation.save
+        UserMailer.reservation_accepted_email(reservation)
+    end
+
 end
