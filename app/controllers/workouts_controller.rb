ea1da03e5caf277c0fb83ea1bb0f4e0fd61408be
splmloic <l2ct@protonmail.com>
2024-03-20 19:53:31 +0100

    Location can be anything, city must be already created

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -10,9 +10,21 @@ class WorkoutsController < ApplicationController
   end
 
   def new
+    @workout = Workout.new
+    @cities = City.all
   end
   
   def create
-    
+    @workout = Workout.create(workout_params)
+    if @workout.save
+      redirect_to @workout, notice: 'La séance a été créé avec succès.'
+    else
+      puts @workout.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+  private
+  def workout_params
+    params.require(:workout).permit(:title, :start_date, :end_date, :participant_number, :description, :price, :location, :city_id, :host_id)
   end
 end
