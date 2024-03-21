bae9514af9b7d019304f8e86b73ab6d484bbddf4
BrvAlexis <brevieralexis@gmail.com>
2024-03-21 22:50:33 +0100

    preview ok

--- test/mailers/previews/host_mailer_preview.rb
+++ test/mailers/previews/host_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/host_mailer
 class HostMailerPreview < ActionMailer::Preview
+    
+    def cancellation_notification
+        reservation = Reservation.first # ou toute autre réservation que vous souhaitez utiliser pour la prévisualisation
+        HostMailer.cancellation_notification(reservation)
+    end
+
 
 end
