1e2d67ba7aeaa0b109e9674515d60b81ed9dee9a
BrvAlexis <brevieralexis@gmail.com>
2024-03-26 12:32:58 +0100

    message déjà soumis un commentaire pour cette réservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,11 +6,12 @@ class ReservationsController < ApplicationController
 
 
   
-
   def index
     @reservations = current_user.reservations.order(created_at: :desc)
     
+    
   end
+  
 
   def show
     @user = User.find(params[:user_id])
