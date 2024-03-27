9eb4546e79e0c5897b5971bc86aeec1a8c36fd67
BrvAlexis <brevieralexis@gmail.com>
2024-03-27 12:32:06 +0100

    note moyenne sur workout/show ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,18 +18,21 @@ class User < ApplicationRecord
   def average_rating
     ratings = reservations.pluck(:user_rating, :host_rating).flatten.compact
     return "Pas encore d'évaluation" if ratings.empty?
-    (ratings.sum.to_f / ratings.size).round(2)
+    (ratings.sum.to_f / ratings.size).round(3)
   end
 
   #pluck récupère les valeurs des colonnes user_rating & host_rating
   #flatten assemble les deux colonnes en une
   #compact supprime les élément nil des colonnes pour ne pas faire baisser la moyenne
+  # sum additionne tous les élément du tableau ratings
+  #/
+  # size calcule le nombre d'élément dans le tableau
   
-  #def average_user_rating
-    #user_ratings = reservations.pluck(:user_rating).compact
-    #return "Pas encore d'évaluation" if user_ratings.empty?
-    #user_ratings.sum / user_ratings.size.to_f
-  #end
+  def average_user_rating
+    user_ratings = reservations.pluck(:user_rating).compact
+    return "Pas encore d'évaluation" if user_ratings.empty?
+    (user_ratings.sum.to_f / user_ratings.size).round(3)
+  end
 
   #def average_host_rating
     #host_ratings = reservations.pluck(:host_rating).compact
