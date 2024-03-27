4924ba25b6cf2fee354d9a44fb2a43e5435ee39f
BrvAlexis <brevieralexis@gmail.com>
2024-03-27 22:17:40 +0100

    test methode controller user/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @workouts = @user.hosted_workouts
+    @average_user_rating = @user.reservations.average(:user_rating)
+    @average_host_rating = @user.reservations.average(:host_rating)
     
   end
 
