01ddde50e7babc44712f8d354ea2900d05ccbfc2
splmloic <l2ct@protonmail.com>
2024-03-28 16:28:46 +0100

    add validate to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,11 @@
 class Contact < ApplicationRecord
     belongs_to :user
+
+    validates :issue,
+    presence: true,
+    length: { in: 4..140 }
+
+  validates :message,
+    presence: true,
+    length: { in: 20..1000 }
 end
