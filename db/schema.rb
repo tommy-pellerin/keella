18f16f896be63e2fb90a5b93af7a1953f2619705
lamrabt <lamrabtyassin3@gmail.com>
2024-03-28 18:28:42 +0100

    rename titre + meta description 3

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_28_110300) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "contacts", force: :cascade do |t|
     t.string "issue"
     t.text "message"
