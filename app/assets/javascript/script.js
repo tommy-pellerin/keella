ad18e7bb9e6d3544516d3397f2f0da90578b0a2f
tommy-pellerin <tommytp@hotmail.fr>
2024-03-29 13:17:25 +0100

    js ok

--- app/assets/javascript/script.js
+++ app/assets/javascript/script.js
@@ -23,24 +23,3 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
-
-
-//Javascript in workout show page
-let reservationBox = document.getElementById('step-before-reservation');
-console.log(reservationBox);
-let reservationButton = document.getElementById('reservation-button');
-console.log(reservationButton);
-let showReservation = function(){
-  if (reservationBox.classList.contains("d-none")){
-    reservationBox.classList.remove("d-none");
-    reservationButton.textContent = 'Annuler';
-    reservationButton.classList.remove("btn-primary");
-    reservationButton.classList.add("btn-danger");
-  } else {
-    reservationBox.classList.add("d-none");  
-    reservationButton.textContent = 'Reserver';
-    reservationButton.classList.remove("btn-danger");
-    reservationButton.classList.add("btn-primary");
-  }
-};
-reservationButton.addEventListener("click",showReservation);
